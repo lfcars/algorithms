7516d585378fcaae73522941b2beac26312f5efa
Filipe Simões <lfcars@gmail.com>
2022-04-02 15:22:11 +0100

    merge-sort

---
+++ merge-sort.js
@@ -0,0 +1,30 @@
+function mergeSortTopDown(array) {
+  if (array.length <= 1) return array;
+
+  const middle = Math.floor(array.length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+
+  return mergeTopDown(mergeSortTopDown(left), mergeSortTopDown(right));
+}
+
+function mergeTopDown(left, right) {
+  const array = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      array.push(left.shift());
+    } else {
+      array.push(right.shift());
+    }
+  }
+  return array.concat(left.slice()).concat(right.slice());
+}
+
+(function test() {
+  console.log("test");
+  const testArray1 = [4, 5, 2, 1, 3, 8];
+  const testArray2 = [72, 3, 1233, -4, 32, 98.32, 5237, 4, 2];
+  console.log(mergeSortTopDown(testArray1));
+  console.log(mergeSortTopDown(testArray2));
+})();
